d8d42844107d6ac0cd6ad049d789d8df6e36b881
Prabhashperera <prabashmperera01@gmail.com>
2026-04-02 19:55:07 +0530

    Created : zone-management-service (v1)

---
+++ zone-management-service/src/test/java/com/project/zonemanagementservice/ZoneManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.zonemanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoneManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
