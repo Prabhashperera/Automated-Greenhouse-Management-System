afb8ae78e887548fc9aebf58602537cb206044fc
Prabhashperera <prabashmperera01@gmail.com>
2026-04-07 21:16:15 +0530

    Created : Crop Inventory Serivice INIT (Entity Crop STatus v2)

---
+++ crop-inventory-service/src/main/java/com/project/cropinventoryservice/entity/CropStatus.java
@@ -0,0 +1,6 @@
+package com.project.cropinventoryservice.entity;
+
+// src/main/java/com/project/cropinventoryservice/entity/CropStatus.java
+public enum CropStatus {
+    SEEDLING, VEGETATIVE, HARVESTED
+}
