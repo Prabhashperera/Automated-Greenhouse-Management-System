c1dc4dd19faf659765681a4dcce4176f694680f3
Prabhashperera <prabashmperera01@gmail.com>
2026-04-07 18:54:06 +0530

    Created : Automation Repo v1 (Automation service changed to persist data, History Endpoint) COMPLETED BACKEND

--- automation-service/src/main/java/com/project/automationservice/controller/AutomationController.java
+++ automation-service/src/main/java/com/project/automationservice/controller/AutomationController.java
@@ -1,23 +1,30 @@
 package com.project.automationservice.controller;
 
 import com.project.automationservice.dto.SensorDataDTO;
+import com.project.automationservice.entity.AutomationAction;
+import com.project.automationservice.repo.AutomationRepo;
 import com.project.automationservice.service.AutomationService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/automation")
+@AllArgsConstructor
 public class AutomationController {
     private final AutomationService automationService;
-
-    public AutomationController(AutomationService automationService) {
-        this.automationService = automationService;
-    }
+    private final AutomationRepo automationRepo;
 
     @PostMapping("/process")
     public void receiveTelemetry(@RequestBody SensorDataDTO data) {
         automationService.processData(data);
+    }
+
+    // Add this to your existing AutomationController
+    @GetMapping("/history")
+    public ResponseEntity<List<AutomationAction>> getHistory() {
+        return ResponseEntity.ok(automationRepo.findAll());
     }
 }
